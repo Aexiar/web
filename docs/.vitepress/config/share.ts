4209be059cd9eae9518b1597506bb662328ed5d6
许大仙 <1900919313@qq.com>
2025-03-06 10:09:28 +0800

    feat(docs): 为 VitePress 配置 Algolia 搜索
    - 在 sharedConfig 中添加 search 配置项，使用 Algolia 作为搜索提供商
    - 配置 Algolia 搜索的选项，包括 appId、apiKey 和 indexName
    - 添加中文搜索相关的翻译文本，涵盖搜索框、结果屏幕、错误屏幕等
    - 配置搜索历史和收藏功能的翻译文本

--- docs/.vitepress/config/share.ts
+++ docs/.vitepress/config/share.ts
@@ -148,5 +148,56 @@ export const sharedConfig = defineConfig({
       { icon: 'github', link: 'https://github.com/Aurorxa/web-design' },
     ],
     externalLinkIcon: true,
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: 'BW18RW01T7',
+        apiKey: 'a0afcb790fe34e4403843e75dc548496',
+        indexName: 'web-design-weiweixu',
+        locales: {
+          zh: {
+            placeholder: '搜索文档',
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消'
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除'
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接'
+                },
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                  searchByText: '搜索提供者'
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  reportMissingResultsText: '你认为该查询应该有结果？',
+                  reportMissingResultsLinkText: '点击反馈'
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   }
 })
